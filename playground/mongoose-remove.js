b2ae5b10b8b7fe92e7044be0400b8159bb8ad1e1
Ha Hoang <hah@Has-MacBook-Air.local>
2017-08-30 16:13:36 +0700

    add route delete

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,9 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {ObjectID} = require('mongodb');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({})
+Todo.remove({}).then((result) => {
+  console.log(result);
+});
--- server/server.js
+++ server/server.js
@@ -56,6 +56,24 @@ app.get('/todos', (req, res) => {
   });
 });
 
+// delete
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send('invalid id');
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send('not todo');
+    }
+    res.status(200).send({todo});
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
